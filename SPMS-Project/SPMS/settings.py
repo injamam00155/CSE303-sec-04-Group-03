1f0b6c3b36485061efdad3dacf609e00d18748f1
injamam00155 <injamam00155@gmail.com>
2022-11-23 02:13:28 +0600

    connected mysql

--- SPMS-Project/SPMS/settings.py
+++ SPMS-Project/SPMS/settings.py
@@ -73,12 +73,19 @@ WSGI_APPLICATION = 'SPMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'spms',  
+        'USER': 'root',  
+        'PASSWORD': 'inja',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
+}  
 
 
 # Password validation
