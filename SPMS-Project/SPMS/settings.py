14a33e3dd88a77c0bc1d9038ec4e1b0e3fdcc9e3
injamam00155 <injamam00155@gmail.com>
2022-12-01 22:33:48 +0600

    fixed something

--- SPMS-Project/SPMS/settings.py
+++ SPMS-Project/SPMS/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'SPMS.wsgi.application'
 DATABASES = {  
     'default': {  
         'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'new_schema',  
+        'NAME': 'spms',  
         'USER': 'root',  
-        'PASSWORD': 'jjk@2030183@jj',  
+        'PASSWORD': 'inja',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
           
