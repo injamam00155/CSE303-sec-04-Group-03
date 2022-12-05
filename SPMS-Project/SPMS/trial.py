11e271474e2b0499f74bfbfc2d1cf73c1cff642d
isti2415 <2021462@iub.edu.bd>
2022-12-06 04:35:28 +0600

    ki jani hoitese

--- SPMS-Project/SPMS/trial.py
+++ SPMS-Project/SPMS/trial.py
@@ -3,13 +3,14 @@ import dbConnection
 
 mydb=dbConnection.queriesDB()
 
-username=1416455
-cursor = mydb.cursor()
-cursor.execute('''        
-SELECT grp    
-FROM spms_users_t
-WHERE userID={}'''.format(username))
-group=cursor.fetchall()
-cursor.close()
 
-print(group)
+cursor = mydb.cursor()
+cursor.execute('''SELECT userID,grp FROM spms_currsess_t''')
+try:
+    rows=cursor.fetchall()
+    cursor.close()
+except:
+    cursor.close()
+    
+
+print(rows)
