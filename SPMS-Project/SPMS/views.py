0e0ad31acd902dc939f489e18dcf8d90e18d52e4
injamam00155 <injamam00155@gmail.com>
2022-12-05 21:08:41 +0600

    navside

--- SPMS-Project/SPMS/views.py
+++ SPMS-Project/SPMS/views.py
@@ -13,7 +13,7 @@ from plotly.offline import plot
 def home(request):
     plot_div = OneTraceSpider([1,2,3,4,5,6],["banna","inja","jaima","niaz","akib","faiza"])
 
-    return render(request,"sHome.html", context={"plot1":plot_div,
+    return render(request,"Student/sHome.html", context={"plot1":plot_div,
                                                 "page":"dashboard",
                                                 "id":queries.getCurrUser()[0],
                                                 "group":queries.getCurrUser()[1],
@@ -40,73 +40,73 @@ def logout(request):
     return render(request,"login.html")
 
 def dashboard(request):
-    return render(request,"sHome.html")
+    return render(request,"Student/sHome.html")
 
 def CoPloAnal(request):
     # queries.getStudentCourseWiseCO(queries.getCurrUser()[0],)
-    return render(request,"co-plo-analysis.html",{"page":"coplo",
+    return render(request,"Student/co-plo-analysis.html",{"page":"coplo",
                                                 "page":"dashboard",
                                                 "id":queries.getCurrUser()[0],
                                                 "group":queries.getCurrUser()[1],
                                                 "name":queries.getName(str(queries.getCurrUser()[0]))})
 
 def coursePloAnal(request):
-    return render(request,"course-plo-analysis.html",{"page":"course",
+    return render(request,"Student\course-plo-analysis.html",{"page":"course",
                                                 "page":"dashboard",
                                                 "id":queries.getCurrUser()[0],
                                                 "group":queries.getCurrUser()[1],
                                                 "name":queries.getName(str(queries.getCurrUser()[0]))})
 
 def PloAchievement(request):
-    return render(request,"PloAchievement.html",{"page":"plo",
+    return render(request,"Student\PloAchievement.html",{"page":"plo",
                                                 "page":"dashboard",
                                                 "id":queries.getCurrUser()[0],
                                                 "group":queries.getCurrUser()[1],
                                                 "name":queries.getName(str(queries.getCurrUser()[0]))})
 
 def QuestionBank(request):
-    return render(request,"QuestionBank.html",{"page":"ques",
+    return render(request,"Student\QuestionBank.html",{"page":"ques",
                                                 "page":"dashboard",
                                                 "id":queries.getCurrUser()[0],
                                                 "group":queries.getCurrUser()[1],
                                                 "name":queries.getName(str(queries.getCurrUser()[0]))})
 
 def StuPloAnal(request):
-    return render(request,"StuPloAnal.html",{"page":"stuplo-anal",
+    return render(request,"Faculty\StuPloAnal.html",{"page":"stuplo-anal",
                                                 "page":"dashboard",
                                                 "id":queries.getCurrUser()[0],
                                                 "group":queries.getCurrUser()[1],
                                                 "name":queries.getName(str(queries.getCurrUser()[0]))})
 
 def StuPloTbl(request):
-    return render(request,"StuPloTbl.html",{"page":"stuplo-tbl",
+    return render(request,"Faculty\StuPloTbl.html",{"page":"stuplo-tbl",
                                                 "page":"dashboard",
                                                 "id":queries.getCurrUser()[0],
                                                 "group":queries.getCurrUser()[1],
                                                 "name":queries.getName(str(queries.getCurrUser()[0]))})
 
 def CourseReport(request):
-    return render(request,"CourseReport.html",{"page":"coursereport",
+    return render(request,"Faculty\CourseReport.html",{"page":"coursereport",
                                                 "page":"dashboard",
                                                 "id":queries.getCurrUser()[0],
                                                 "group":queries.getCurrUser()[1],
                                                 "name":queries.getName(str(queries.getCurrUser()[0]))})
 
 def QuestionBank(request):
-    return render(request,"QuestionBank.html",{"page":"quesentry",
+    return render(request,"Student\QuestionBank.html",{"page":"quesentry",
                                                 "page":"dashboard",
                                                 "id":queries.getCurrUser()[0],
                                                 "group":queries.getCurrUser()[1],
                                                 "name":queries.getName(str(queries.getCurrUser()[0]))})
 def QuestionBankEntry(request):
-    return render(request,"QuestionBankEntry.html",{"page":"quesentry",
+    return render(request,"Faculty\QuestionBankEntry.html",{"page":"quesentry",
                                                 "page":"dashboard",
                                                 "id":queries.getCurrUser()[0],
                                                 "group":queries.getCurrUser()[1],
                                                 "name":queries.getName(str(queries.getCurrUser()[0]))})
 
 def COentry(request):
-    return render(request,"COentry.html",{"page":"coentry",
+    return render(request,"Faculty\COentry.html",{"page":"coentry",
                                                 "page":"dashboard",
                                                 "id":queries.getCurrUser()[0],
                                                 "group":queries.getCurrUser()[1],
