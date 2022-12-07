4dcfc63cf6aa42e646cc35013309e29fcc11d617
isti2415 <2021462@iub.edu.bd>
2022-12-07 06:49:07 +0600

    here you go

--- SPMS-Project/SPMS/queries.py
+++ SPMS-Project/SPMS/queries.py
@@ -18,7 +18,13 @@ def fetchQuestions(course_id,section_id,assessment,semester):
     and semester="{}";'''.format(section_id,course_id,assessment,semester))
     rows=cursor.fetchall()
     cursor.close()
-    # return [[][][][]]
+    QuestionBank=[[]for i in range(4)]
+    for i in range(len(rows)):
+        QuestionBank[0].append(rows[i][0])
+        QuestionBank[1].append(rows[i][1])
+        QuestionBank[2].append(rows[i][2])
+        QuestionBank[3].append(rows[i][3])
+    return QuestionBank
     
 
 
